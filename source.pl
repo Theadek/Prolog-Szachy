0cd6f3d983683879cece5c85591e07855f0cd686
FFugi <fudifil@gmail.com>
2019-03-06 14:40:26 +0100

    add white pawn move definitions

--- source.pl
+++ source.pl
@@ -16,9 +16,8 @@ example([[e, [n, w], e, e, [b, b], e],
          [e, [p, w], [n, w], e, e, e]]).
 
 % zrwaca figurę, B - plansza
-get_chessman(B, X, Y, F) :- ch2num(X, XN), 
-                            XI is XN - 1, nth0(XI, B, L), 
-                            YI is Y - 1, nth0(YI, L, F).
+get_chessman(B, X, Y, C) :- nth0(X, B, L), 
+                            nth0(Y, L, C).
 
 % zmiana litery na cyfrę
 ch2num(a, 1).
@@ -28,10 +27,30 @@ ch2num(d, 4).
 ch2num(e, 5).
 ch2num(f, 6).
 
+% biały pion do przodu X, Y -> indeksy
+possible_move(B, X, Y, 0, 1) :- get_chessman(B, X, Y, [Ch, Co|_]), 
+                                Ch = p, Co = w,
+                                TX is X, TY is Y + 1,
+                                get_chessman(B, TX, TY, E), 
+                                E = e.
+% biały pion do przodu, w prawo X, Y -> indeksy
+possible_move(B, X, Y, 1, 1) :- get_chessman(B, X, Y, [Ch, Co|_]), 
+                                Ch = p, Co = w,
+                                TX is X + 1, TY is Y + 1,
+                                get_chessman(B, TX, TY, [_, ECo|_]), 
+                                ECo = b.
+% biały pion do przodu, w lewo X, Y -> indeksy
+possible_move(B, X, Y, -1, 1) :- get_chessman(B, X, Y, [Ch, Co|_]), 
+                                Ch = p, Co = w,
+                                TX is X - 1, TY is Y + 1,
+                                get_chessman(B, TX, TY, [_, ECo|_]), 
+                                ECo = b.
+
+
 % sprawdzanie, czy pozycje znajdują się na planszy
 pos(X1, Y1, X2, Y2) :- Y1 =< 6, Y1 >= 1,
                        Y2 =< 6, Y2 >= 1,
-                       ch2num(X1, X1N), ch2num(X2, X2N).
+                       ch2num(X1, _), ch2num(X2, _).
 
 % TODO:
 % zdefiniować ruchy figur,
