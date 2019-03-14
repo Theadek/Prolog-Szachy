67190bbaa350161f57ede467907fffdd4ca3851f
Theadek <theadek10@gmail.com>
2019-03-14 11:31:15 +0100

    remove unnecessary function

--- source.pl
+++ source.pl
@@ -1,10 +1,3 @@
-board(_,[],_).
-board([],_,_).
-board([A],[B],L) :- L = [[A,B]].
-board([A],[B|D],L) :- append([[A,B]],L1,L),board([A],D,L1).
-board([A|C],[B|D],L) :- board([A],[B|D],L1),board(C,[B|D],L2),append(L1,L2,L).
-
-
 % przyjmujemy że oś X szachownicy to litery, a Y to cyfry 
 
 % zwraca przykładową planszę z zadania
